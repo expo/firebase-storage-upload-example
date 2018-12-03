d9eb3ef0596725b4a1156efc7b02ec791a6094d6
Stanisław Chmiela <sjchmiela@gmail.com>
2018-12-03 15:25:46 +0100

    Update to SDK31

--- App.js
+++ App.js
@@ -177,13 +177,30 @@ export default class App extends React.Component {
 }
 
 async function uploadImageAsync(uri) {
-  const response = await fetch(uri);
-  const blob = await response.blob();
+  // Why are we using XMLHttpRequest? See:
+  // https://github.com/expo/expo/issues/2402#issuecomment-443726662
+  const blob = await new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+      resolve(xhr.response);
+    };
+    xhr.onerror = function(e) {
+      console.log(e);
+      reject(new TypeError('Network request failed'));
+    };
+    xhr.responseType = 'blob';
+    xhr.open('GET', uri, true);
+    xhr.send(null);
+  });
+
   const ref = firebase
     .storage()
     .ref()
     .child(uuid.v4());
-
   const snapshot = await ref.put(blob);
-  return snapshot.downloadURL;
+
+  // We're done with the blob, close and release it
+  blob.close();
+
+  return await snapshot.ref.getDownloadURL();
 }
